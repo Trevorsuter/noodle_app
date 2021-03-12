0b54ab423dbfac556feefaea74410f9644755c42
Trevor Suter <Trevorsuter@icloud.com>
2021-03-12 01:34:31 -0700

    gemfiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/', to: 'welcome#index', as: 'welcome'
+  root 'welcome#index'
   resources :users, only: [:show, :new] do
     scope module: :user do
       resources :dashboard, only: [:index]

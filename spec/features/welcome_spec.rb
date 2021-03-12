60ca90e173635a11d23fed8cfe5dd631d307e683
Trevor Suter <Trevorsuter@icloud.com>
2021-03-11 23:45:03 -0700

    added new user test in welcome spec

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.describe 'welcome page', type: :feature do
+  before :each do
+    @user1 = User.create!(name: "Trevor Suter", email: "Trevorsuter@icloud.com", password: "Gosharks1", birthday: Date.new(1997, 2, 1))
+    visit "/"
+  end
+
+  it 'has an option to create a new user' do
+    
+    expect(page).to have_Link("Create New User")
+  end
+
+  it 'has an option to login'
+end

9a994494bb837d776193fd956e0826f41ad8c024
Trevor Suter <Trevorsuter@icloud.com>
2021-03-14 00:24:44 -0700

    added links routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create] do
     scope module: :user do
       resources :dashboard, only: [:index]
+      resources :lists
     end
   end
 

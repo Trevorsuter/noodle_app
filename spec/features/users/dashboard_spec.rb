6a47b20b22b22f327421824621213c04ec224f14
Trevor Suter <Trevorsuter@icloud.com>
2021-03-17 16:06:40 -0600

    added tests for the dashboard

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -24,7 +24,36 @@ RSpec.describe 'user dashboard index spec', type: :feature do
   end
 
   it 'has a form to add a partner when one isnt assigned' do
-    expect(page).to have_field('partner_request[partner_email]')
+    expect(page).to have_field('partner_email')
     expect(page).to have_button("Request")
+  end
+
+  it 'can submit a request for a partner' do
+    fill_in 'partner_email', with: 'Alexissayles@colorado.edu'
+    click_button 'Request'
+
+    expect(page).to have_content("Partner Request submitted! Waiting on their approval.")
+    expect(current_path).to eq(user_dashboard_index_path(@user1.id))
+  end
+
+  it 'can field a request from a partner' do
+    PartnerRequest.create!(user: @user2, partner: @user1)
+    visit user_dashboard_index_path(@user1)
+
+    expect(page).to have_content("You have a new request from #{@user2.name}!")
+    expect(page).to have_button("Accept")
+    expect(page).to have_button("Reject")
+  end
+
+  it 'can accept a request' do
+    PartnerRequest.create!(user: @user2, partner: @user1)
+    visit user_dashboard_index_path(@user1)
+    click_button 'Accept'
+
+    expect(page).to_not have_field('partner_email')
+    expect(page).to_not have_button('Request')
+    expect(page).to have_content(@user2.name)
+    expect(page).to have_content(@user2.birthday)
+    expect(page).to have_content(@user2.email)
   end
 end

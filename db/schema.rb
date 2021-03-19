a2d8395096dc41194f359a3412f07b0337b44e49
Trevor Suter <Trevorsuter@icloud.com>
2021-03-18 20:48:40 -0600

    added competitions tto schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_231910) do
+ActiveRecord::Schema.define(version: 2021_03_18_234501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.integer "point_limit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "list_tasks", force: :cascade do |t|
     t.bigint "list_id"
